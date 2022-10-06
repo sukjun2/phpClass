d88426f7ad2d0742322f04911e48bcd90c12c5b9
tjrwns3650 <tjrwns3650@gmail.com>
2022-10-06 14:11:28 +0900

    221006

--- htdocs/board/board.php
+++ htdocs/board/board.php
@@ -114,6 +114,8 @@
                 echo "<td>".$info['boardView']."</td>";
                 echo "</tr>";
             }
+        } else {
+            echo "<tr><td colspan='4'>게시글이 없습니다.</td></tr>";
         }
     }
 ?>                            
