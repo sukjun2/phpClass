2889fb64db19ebfa69092c5735c5a09f9b8fbb59
tjrwns3650 <tjrwns3650@gmail.com>
2022-10-06 15:30:12 +0900

    221006

--- htdocs/board/boardSearch.php
+++ htdocs/board/boardSearch.php
@@ -29,16 +29,21 @@
             <h2>게시판 영역입니다.</h2>
             <div class="board__inner">
                 <div class="board__title">
-                    <h3>검색 결과 게시판</h3>
-                </div>
+                    <h3>검색 결과 게시판</h3>      
 <?php
+    if(isset($_GET['page'])){
+        $page = (int) $_GET['page'];
+    } else {
+        $page = 1;
+    }
+
     function msg($alert){
         echo "<p>총 ".$alert."건이 검색되었습니다.</p>";
     }
 
     $searchKeyword = $_GET['searchKeyword'];
     $searchOption = $_GET['searchOption'];
-    
+
     $searchKeyword = $connect -> real_escape_string(trim($searchKeyword));
     $searchOption = $connect -> real_escape_string(trim($searchOption));
 
@@ -55,13 +60,13 @@
             $sql .= "WHERE m.youName LIKE '%{$searchKeyword}%' ORDER BY myBoardID DESC ";
             break;
     }
-
     $result = $connect -> query($sql);
-    
+
     // 전체 게시글 갯수
-    $count = $result -> num_rows;
-    msg($count);
+    $totalCount = $result -> num_rows;
+    msg($totalCount);
 ?>
+                </div>
                 <div class="board__table">
                     <table>
                         <colgroup>
@@ -82,41 +87,78 @@
                         </thead>
                         <tbody>
 <?php
-    if(isset($_GET['page'])){
-        $page = (int)$_GET['page'];
-    } else {
-        $page = 1;
-    }
-
     $viewNum = 10;
     $viewLimit = ($viewNum * $page) - $viewNum;
 
     $sql = $sql."LIMIT {$viewLimit}, {$viewNum}";
     $result = $connect -> query($sql);
 
+    if($result){
+        $count = $result -> num_rows;
 
-    if($count > 0){
-        for($i=1; $i <= $count; $i++){
-            $info = $result -> fetch_array(MYSQLI_ASSOC);
-            echo "<tr>";
-            echo "<td>".$info['myBoardID']."</td>";
-            echo "<td><a href='boardView.php?myBoardID={$info['myBoardID']}'>".$info['boardTitle']."<a/></td>";
-            echo "<td>".$info['youName']."</td>";
-            echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
-            echo "<td>".$info['boardView']."</td>";
-            echo "</tr>";
+        if($count > 0){
+            for($i=1; $i <= $count; $i++){
+                $info = $result -> fetch_array(MYSQLI_ASSOC);
+                echo "<tr>";
+                echo "<td>".$info['myBoardID']."</td>";
+                echo "<td><a href='boardView.php?myBoardID={$info['myBoardID']}'>".$info['boardTitle']."</td>";
+                echo "<td>".$info['youName']."</td>";
+                echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
+                echo "<td>".$info['boardView']."</td>";
+                echo "</tr>";
             }
         } else {
             echo "<tr><td colspan='4'>게시글이 없습니다.</td></tr>";
         }
-?>                      </tbody>
+    }
+?>
+                        </tbody>
                     </table>
                 </div>
+
                 <div class="board__pages">
                     <ul>
 <?php
+    // echo $totalCount;
 
-?>                        
+    // 총 페이지 갯수
+    $boardCount = ceil($totalCount/$viewNum);
+
+    // echo $boardCount;
+
+    // 현재 페이지를 기준으로 보여주고 싶은 갯수
+    $pageCurrent = 5;
+    $startPage = $page - $pageCurrent;
+    $endPage = $page + $pageCurrent;
+
+    // 처음 페이지 초기화
+    if($startPage < 1) $startPage = 1;
+
+    // 마지막 페이지 초기화 
+    if($endPage >= $boardCount) $endPage = $boardCount;
+
+    // 이전 페이지, 처음 페이지
+    if($page != 1){
+        $prevPage = $page - 1;
+        echo "<li><a href='boardSearch.php?page=1&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>처음으로</a></li>";
+        echo "<li><a href='boardSearch.php?page={$prevPage}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>이전</a></li>";
+    }
+
+    // 페이지 넘버 표시
+    for($i=$startPage; $i<=$endPage; $i++){
+        $active = "";
+        if($i == $page) $active = "active";
+        
+        echo "<li class='{$active}'><a href='boardSearch.php?page={$i}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>{$i}</a></li>";
+    }
+
+    // 다음 페이지, 마지막 페이지
+    if($page != $endPage){
+        $nextPage = $page + 1;
+        echo "<li><a href='boardSearch.php?page={$nextPage}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>다음</a></li>";
+        echo "<li><a href='boardSearch.php?page={$boardCount}&searchKeyword={$searchKeyword}&searchOption={$searchOption}'>마지막으로</a></li>";
+    }
+?>
                     </ul>
                 </div>
             </div>
