26958e6698b450716a8762c78689fdf0cc6ed85d
sukjun2 <109261945+sukjun2@users.noreply.github.com>
2022-10-11 14:13:38 +0900

    221011

--- htdocs/include/footer.php
+++ htdocs/include/footer.php
@@ -2,7 +2,10 @@
     <h2>푸터 영역입니다.</h2>
     <div class="footer__inner container">
         <address>Copyright @2022 corturtle</address>
-        <div>blog by corturtle</div>
+        <div>
+            blog by corturtle |   
+            <a href="../admin/adminJoin.php">관리자 로그인</a>
+        </div>
     </div>
 </footer>
 <!-- //footer -->
